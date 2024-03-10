f8ff1f62656a33b2745c020b2d45ab45f4b526de
lucasttmz <lucasttmz@gmail.com>
2024-03-09 23:01:31 -0300

    Reestilização da mesa

--- main.lua
+++ main.lua
@@ -97,7 +97,6 @@ function love.update(dt)
     elseif estadoAtual == "reposição" then
         realocarBolaBranca()
     end
-
 end
 
 function love.mousepressed(x, y, button)
@@ -150,23 +149,45 @@ function desenharBola(bola)
 end
 
 function desenharMesa()
-    -- Bordas
+    -- Madeira da borda
     love.graphics.setColor(64/255, 29/255, 8/255)
-    love.graphics.rectangle("fill", 
+    love.graphics.rectangle(
+        "fill", 
         mesa.x - (RAIO_BURACO * 2), 
         mesa.y - (RAIO_BURACO * 2), 
         mesa.largura + (RAIO_BURACO * 4), 
         mesa.altura + (RAIO_BURACO * 4)
     )
 
+    -- Camadas de madeira
+    love.graphics.setColor(1, 1, 1, 0.1)
+    love.graphics.rectangle("fill", 80, 15, mesa.largura + 80, mesa.altura + 90)
+    love.graphics.setColor(64/255, 29/255, 8/255)
+    love.graphics.rectangle("fill", 100, 40, mesa.largura + 40, mesa.altura + 40)
+   
+    -- Protetor da borda
+    love.graphics.setColor(1, 1, 1, 0.8)
+    love.graphics.setLineWidth(3)
+    for _, linha in pairs(bordas) do
+        love.graphics.line(linha.x1, linha.y1, linha.x2, linha.y2)
+    end
+
     -- Tecido
-    love.graphics.setColor(0, 1, 0, 0.8)
+    love.graphics.setColor(0, 1, 0, 0.5)
     love.graphics.rectangle("fill", mesa.x, mesa.y, mesa.largura, mesa.altura)
 
     -- Buracos
-    love.graphics.setColor(0,0,0)
     for _, buraco in pairs(buracos) do
+        love.graphics.setColor(1, 1, 1, 1)
         love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO)
+
+        -- Textura
+        love.graphics.setColor(0 ,0, 0, 0.5)
+        love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO)
+        love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO * 0.90)
+        love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO * 0.75)
+        love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO * 0.50)
+        love.graphics.circle("fill", buraco.x, buraco.y, RAIO_BURACO * 0.25)
     end
 end
 
